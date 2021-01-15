a058215b5d3981e37eea50b595283aa0c2566057
Angel Bulnes <angel_bulnes16@yahoo.com>
2021-01-14 21:11:45 -0600

    Agregado componentes base

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Logo from "./app/components/Logo";
+import SearchWeather from "./app/components/SearchWeather";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Logo />
+      <SearchWeather />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
